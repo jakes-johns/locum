6f02425ac342fc86f1ff95de6d00d65a7e93c53b
jakes-johns <jakesjohns29@gmail.com>
2025-04-05 00:04:34 +0300

    adding .env

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
 import os
 from .db import db
 from . import models  # Ensure models are registered
 
+# Load .env from project root
+load_dotenv()
 migrate = Migrate()  # Initialize migrate object
 
 def create_app():
@@ -12,8 +15,8 @@ def create_app():
     app = Flask(__name__)
 
     # Configuration
-    app.config['SECRET_KEY'] = '278389u383uru83u84ui384uiojr3u3iu43i4uojioewri3irinu3i'  # Change to a secure key in production
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://locumdb_owner:npg_BAxjq7pys9aZ@ep-young-cherry-a5c19pys-pooler.us-east-2.aws.neon.tech/locumdb?sslmode=require'
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")  # Change to a secure key in production
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'static', 'uploads')
 
